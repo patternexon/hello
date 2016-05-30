a2b42692bc51e951260f2ddcaca2275a2666e780
patternexon <patternexon@yahoo.com>
2016-05-30 12:31:10 -0400

    Switching to headlines

--- hello.py
+++ hello.py
@@ -1,10 +1,20 @@
+import feedparser
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+RSS_FEEDS = {
+        'hn' : 'https://news.ycombinator.com/rss',
+        'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml'
+        }
+
 @app.route("/")
-def index():
-    return "Hello, World!"
+@app.route("/<publication>")
+def get_news(publication="hn"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
