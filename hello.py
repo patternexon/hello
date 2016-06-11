04fe7161f2a9998f8f15f7cb954054d6bfa6b00b
patternexon <patternexon@yahoo.com>
2016-06-11 13:47:41 -0400

    Multiple feeds on the same page

--- hello.py
+++ hello.py
@@ -10,11 +10,17 @@ RSS_FEEDS = {
         }
 
 @app.route("/")
+def get_all():
+    feeds = ""
+    for feedname in RSS_FEEDS:
+        feeds = feeds + get_news(feedname)
+    return render_template("home.html", feeds=feeds)
+
 @app.route("/<publication>")
 def get_news(publication="hn"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return render_template("home.html", articles=feed['entries'])
+#    first_article = feed['entries'][0]
+    return render_template("feed.html", title=feed['feed']['title'], articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
